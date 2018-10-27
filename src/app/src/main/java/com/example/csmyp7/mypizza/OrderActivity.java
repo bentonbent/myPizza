4a4e90d2203568c2fc14543aa9e3a27d743b363f
cs1mccullough <42701800+cs1mccullough@users.noreply.github.com>
2018-10-27 15:29:22 -0500

    commit
    commit

--- src/app/src/main/java/com/example/csmyp7/mypizza/OrderActivity.java
+++ src/app/src/main/java/com/example/csmyp7/mypizza/OrderActivity.java
@@ -1,7 +1,9 @@
 package com.example.csmyp7.mypizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
     }
+
+    public void mySummary(View v){
+        Intent summary = new Intent(OrderActivity.this, SummaryActivity.class);
+        startActivity(summary);
+    }
+
+   // public String myOrderSummary()
+
 }
--- src/app/src/main/java/com/example/csmyp7/mypizza/SummaryActivity.java
+++ src/app/src/main/java/com/example/csmyp7/mypizza/SummaryActivity.java
@@ -1,7 +1,9 @@
 package com.example.csmyp7.mypizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+    }
+
+    public void myOrder(View v){
+        Intent order = new Intent(SummaryActivity.this, OrderActivity.class);
+        startActivity(order);
     }
 }
